00952dc55a766952987ab393a0f33e5d3d1cafcf
paskalk <paskalk@gmail.com>
2015-04-09 08:58:06 +0300

    revision april 8 - sms

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo'hello ussd'
+        echo'hello ussd';
         // put your code here
         ?>
     </body>
